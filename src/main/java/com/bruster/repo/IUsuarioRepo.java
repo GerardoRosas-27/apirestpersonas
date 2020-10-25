0d4b72a676516c33b174caba4c212f20149cf3f7
GerardoRosas-27 <brustergabriel24@gmail.com>
2020-10-24 21:17:25 -0500

    unitest insertar usuarios

---
+++ src/main/java/com/bruster/repo/IUsuarioRepo.java
@@ -0,0 +1,10 @@
+package com.bruster.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bruster.model.Persona;
+import com.bruster.model.Usuario;
+
+public interface IUsuarioRepo extends JpaRepository<Usuario, Integer> {
+	
+}
--- src/test/java/com/bruster/PersonaapirestApplicationTests.java
+++ src/test/java/com/bruster/PersonaapirestApplicationTests.java
@@ -1,16 +1,32 @@
 package com.bruster;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.bruster.model.Usuario;
+import com.bruster.repo.IUsuarioRepo;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PersonaapirestApplicationTests {
 
+	@Autowired
+	private IUsuarioRepo repo;
+	
 	@Test
-	public void contextLoads() {
+	public void crearUsuarioTest() {
+		Usuario usuario = new Usuario();
+		usuario.setId(1);
+		usuario.setNombre("gerardo");
+		usuario.setClave("123");
+		Usuario retorno = repo.save(usuario);
+		
+		assertTrue(retorno.getClave().equalsIgnoreCase(usuario.getClave()));
 	}
 
 }
